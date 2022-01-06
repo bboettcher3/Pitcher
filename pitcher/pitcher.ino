08a636fc7abf552231810b7635744e7849e79cc9
Brady Boettcher <bboettcher2@wisc.edu>
2022-01-06 15:53:28 -0500

    Initial commit

---
+++ pitcher/pitcher.ino
@@ -0,0 +1,16 @@
+#include <ArduinoJson.h>
+
+void setup() {
+  // Initialize serial port
+  Serial.begin(9600);
+  // Setup button pins
+
+  // Setup joystick pins
+}
+
+void loop() {
+  // Update current scale from joystick values
+  
+  // Check button states, output MIDI when pressed from respective scale
+
+}
